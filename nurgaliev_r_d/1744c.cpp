9c7ae38df00e2888f760ccc9052f14286d75eb48
LLrodyaLL <113860484+LLrodyaLL@users.noreply.github.com>
2022-10-18 15:59:30 +0300

    Create 1744c.cpp

---
+++ nurgaliev_r_d/1744c.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+    int t;
+    std::cin >> t;
+    for (int b = 0; b < t; ++b) {
+        int n;
+        char colour;
+        std::string word;
+        std::cin >> n >> colour >> word;
+    }
+}
